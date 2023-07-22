8430855303466fc22b250248cb44b5c1b6344a96
GhostMiner <66090703+Ghost-Miner@users.noreply.github.com>
2023-07-22 13:24:49 +0200

    a try to fix gothub's 404 with dirs with "_"

--- scripts/insertComponents.js
+++ scripts/insertComponents.js
@@ -1,11 +1,11 @@
 // Script that inject content of "_sidebar.html" in the "mainNavBarDiv" component
 (   async function () 
     {
-        const navHTMLRequest = await fetch('./_parts/sidebar.html'); // REtrueve the file
+        const navHTMLRequest = await fetch('./parts/sidebar.html'); // REtrueve the file
         const navHTML        = await navHTMLRequest.text(); // Extract its content
         document.querySelector('.websiteSideBar').innerHTML = navHTML; // Insert cintent into the selected div
 
-        const footerHTMLRequest = await fetch("./_parts/footer.html");
+        const footerHTMLRequest = await fetch("./parts/footer.html");
         const footerHTML = await footerHTMLRequest.text();
         document.querySelector("#footerDiv").innerHTML = footerHTML;
     } 
